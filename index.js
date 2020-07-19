7a76afbf138e74295cfc706937631a54bdd277d0
gomyo <engcraft1102@gmail.com>
2020-07-19 19:44:43 +0900

    Create user model

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const express = require("express");
 const app = express();
 // port 설정은 자유
 const port = 5000;
+
+const mongoose = require("mongoose")
+mongoose.connect(
+  "mongodb+srv://joonwon:abcd1234@boilerplate.qabqn.mongodb.net/<dbname>?retryWrites=true&w=majority",{
+      useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log("MongoDB Connected...")).catch(err => console.log(err));
+
 // root 디렉토리 ("/")에 오면 Hello world를 출력하라
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World!??왜 이렇게 해야 열림? npm run start가 안되넹ㅋㅋ"));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
